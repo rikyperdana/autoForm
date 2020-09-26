8acb5e800db809e37d94dc37116371a1e45086fe
Riky Perdana <perdanariky@gmail.com>
2020-09-27 01:06:53 +0700

    dedicated app

---
+++ app.js
@@ -0,0 +1,25 @@
+var m, _, autoForm
+
+m.mount(document.body, {view: () =>
+  m('.container', m(autoForm({
+    id: 'insertContact',
+    action: console.log,
+    schema: {
+      name: {type: String, label: 'Full Name'},
+      age: {type: Number, minMax: () => [18, 65]},
+      birth: {type: Date, autoform: {type: 'datetime-local'}},
+      family: {type: Object},
+      'family.father': {type: String},
+      'family.mother': {type: String},
+      siblings: {type: Array},
+      'siblings.$': {type: String}
+    },
+    submit: {value: 'Save', class: 'is-info'},
+    autoReset: true,
+    confirmMessage: 'Are you sure to submit it?',
+    arangement: [
+      ['name', 'age', 'birth'],
+      ['family', 'siblings']
+    ]
+  })))
+})
